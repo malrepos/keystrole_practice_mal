5e72f5f493ceb94c87fc22bf402c2ebb961433e2
Dan Isaacman <106420953+DIsaacman@users.noreply.github.com>
2022-07-17 13:35:49 +1000

    moved_characters

---
+++ char.py
@@ -0,0 +1,26 @@
+#List of common keyboard strokes for programmers
+list_ = { "syntax" :
+            [ "*","(",")","_","+",
+            "{","}","|","[","]","\\",
+            ":",'"',"'",";","'",
+            "<",">","?",",",".","?",
+            "%","#","!","~","`","^","&" 
+            ],
+        
+        "git" :
+            ["cd","cd ~","cd ../","ls","pwd"
+            "touch","echo","mkdir", "git add .",
+            "git commit -m", "git push", "git clone",
+            "git checkout", "git pull", "code", "python",
+            "jupyter lab","conda activate"
+            ],
+
+        "python":
+            ["if","elif","else","for","and","or",   
+            ],
+        
+        "pandas":
+            ["loc","iloc"
+            ]
+    
+}
