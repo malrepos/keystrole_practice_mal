b20d3621068f5e616e0925cd32bb8628d12efe37
Dan <SydneyAudioVisualSup@mgd.mrshmc.com>
2022-07-19 16:51:49 +1000

    alphabetical_order

--- Resources/char.py
+++ Resources/char.py
@@ -1,6 +1,6 @@
-#List of common keyboard strokes for programmers
-
 # Initiate dcitionary of lists with syntax sets. Default is syntax
+
+        #Common Syntax Characters
 list_ = {"syntax": 
             [ "*","(",")","_","+",
             "{","}","|","[","]","\\",
@@ -11,44 +11,37 @@ list_ = {"syntax":
 
         #Common git Commands            
         "git" :
-            ["cd","cd ~","cd ../","ls","pwd"
-            "touch","echo","mkdir", "git add .",
-            "git commit -m", "git push", "git clone",
-            "git checkout", "git pull", "code", "python",
-            "jupyter lab","conda activate","cp", "git status",
-            "git diff", "git config", "git init", "git branch",
-            "git log", "git merge", "git log", "git show",
-            "git fetch", "git remote add", "git rebase", "git rm",
-            "git mv", "git log", "git reset", "git rebase", "git stash"
+            ['cd', 'cd ../', 'cd ~', 'code', 'conda activate', 'cp', 'echo', 
+             'git add .', 'git branch', 'git checkout', 'git clone', 'git commit -m', 
+             'git config', 'git diff', 'git fetch', 'git init', 'git log', 'git log', 
+             'git log', 'git merge', 'git mv', 'git pull', 'git push', 'git rebase', 
+             'git rebase', 'git remote add', 'git reset', 'git rm', 'git show', 
+             'git stash', 'git status', 'jupyter lab', 'ls', 'mkdir', 'mv', 'pwd', 
+             'python', 'touch'
             ],
         
         # Common python functions
         "python":
-            ["if","elif","else","for","break",
-            "and","or","len","input", "range", "in",
-            "str", "int","float", "tuple", "type",
-             "is" , "is not" ,"True","False", "add", "lambda",
-             "pass","while", "print", "from", "import",
-             "def", "return" , ".pop","reverse", "set", ".remove",
-             ".update", ".discard", ".union", ".intersection", "difference",
-             "del","zip", ".values" , ".get", ".setdefault",
-             "+=", "-=","*=","/=","%=","==", "!=", "<=", ">=",
-             ".append",".insert",".remove",".sort","sorted()", "list",
-             "end", "sep" , ".format"
+            ['!=', '%=', '*=', '+=', '-=', '/=', '<=', '==', '>=', 
+             '.append', '.discard', '.format', '.get', '.insert', 
+             '.intersection', '.pop', '.remove', '.remove', '.setdefault', 
+             '.sort', '.union', '.update', '.values', 
+             'False', 'True', 'add', 'and', 'break', 'def', 'del', 'difference', 
+             'elif', 'else', 'end', 'float', 'for', 'from', 'if', 'import', 'in', 
+             'input', 'int', 'is', 'is not', 'lambda', 'len', 'list', 'map', 'or', 
+             'pass', 'print', 'range', 'return', 'reverse', 'sep', 'set', 'sorted()', 
+             'str', 'tuple', 'type', 'while', 'zip'
             ],
         
         #Common PANDAS functionality
         "pandas":
-            ["loc","iloc", "df",
-            ".read_csv" ,".read_table" ,".read_excel" ,".read_sql",
-            ".read_json" ,".read_html" ,".read_clipboard", ".DataFrame",
-            ".to_csv", ".to_excel", ".to_sql", ".to_json"
-            ".Series",".index=",".DataFrame",".columns",
-            ".head", ".tail",".shape",".info",".describe",".value_counts",".apply",
-            ".isnull" ,".notnull" ,".dropna" ,".fillna" ,".astype" ,".replace" ,
-            ".rename" ,".set_index" ,".sort_values" ,".sort_index" ,
-            ".groupby" ,".pivot" ,".apply" ,".append" ,".concat" , ".join",
-            ".describe" ,".mean" ,".corr" ,".count" ,".max" ,
-            ".min" ,".median" ,".std" 
+            ['.DataFrame', '.DataFrame', '.Series', '.append', '.apply', '.astype', 
+             '.columns', '.concat', '.corr', '.count', '.describe', '.describe', '.dropna', 
+             '.fillna', '.groupby', '.head', '.index=', '.info', '.isnull', '.join', '.max', 
+             '.mean', '.median', '.merge', '.min', '.notnull', '.pivot', '.read_clipboard', 
+             '.read_csv', '.read_excel', '.read_html', '.read_json', '.read_sql', 
+             '.read_table', '.rename', '.replace', '.set_index', '.shape', '.shift', 
+             '.sort_index', '.sort_values', '.std', '.tail', '.to_csv', '.to_excel', 
+             '.to_json', '.to_sql', '.value_counts', 'df', 'iloc', 'loc'
             ]
         }
